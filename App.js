3048b0a6f2fbb4d68fa91c94854368c285cb2684
KritikaSr <103661685+KritikaSr@users.noreply.github.com>
2022-09-06 23:54:40 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
+import List from "./components/exercises-list.component";
+import Edit from "./components/edit-exercise.component";
+import Create from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
 function App() {
@@ -14,9 +14,9 @@ function App() {
       <div className="container">
       <Navbar />
       <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
+      <Route path="/" exact component={List} />
+      <Route path="/edit/:id" component={Edit} />
+      <Route path="/create" component={Create} />
       <Route path="/user" component={CreateUser} />
       </div>
     </Router>
