765d04ffb90347d759b4fb5ed4a4d0d4bb30336a
KritikaSr <103661685+KritikaSr@users.noreply.github.com>
2022-09-06 20:09:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-#exercise tracker
-#using mern stack
-#javascript in mern
+//exercise tracker
+//using mern stack
+//javascript in mern
+// exercise tracker is used to track the health and fitness
+//using js would make it easier
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
